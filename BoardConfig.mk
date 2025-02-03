97127e95215a38a97eda784ef2c0b853bbcb573e
Darth9 <reyprimerjefe@gmail.com>
2025-02-03 22:47:36 +0000

    Add vendor prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Copyright (C) 2022-2024 The OrangeFox Recovery Project
+# Copyright (C) 2022-2025 The OrangeFox Recovery Project
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
@@ -132,6 +132,7 @@ BOARD_QTI_DYNAMIC_PARTITIONS_PARTITION_LIST := system product odm system_ext ven
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Recovery
 BOARD_HAS_LARGE_FILESYSTEM := true
